986da9b04a9aa8a5a523817117d640e0b6910f40
Jimmy Chuang <ogre0403@gmail.com>
2022-06-11 15:31:52 +0800

    add timeout warning

--- .validate/validate-rule3.sh
+++ .validate/validate-rule3.sh
@@ -7,12 +7,21 @@ echo "驗證Informer 是否會自動建立Service，並測試是否連連通"
 LABEL="ntcu-k8s=hw3"
 
 # informer 會建立一個svc
-svc_num=`kubectl get svc   -l ${LABEL}  -o yaml | yq '.items | length'`
 
-if [[ "$svc_num" -ne 1 ]]; then
-    echo "informer建立的svc數量 $svc_num 不正確. 應為 1"
-    exit 1
-fi
+for i in {1..20}; do
+  sleep 1
+  svc_num=`kubectl get svc   -l ${LABEL}  -o yaml | yq '.items | length'`
+
+  if [[ "$svc_num" -eq 1 ]]; then
+      break
+  fi
+
+  if [[ "$i" -eq 20 ]]; then
+      echo "timeout: informer建立的svc數量 $svc_num 不正確. 應為 1"
+      exit 1
+  fi
+
+done
 
 cid=`docker ps -f name=control-plane -q`
 
--- .validate/validate-rule4.sh
+++ .validate/validate-rule4.sh
@@ -3,26 +3,25 @@
 echo ""
 echo "測試刪除nginx Deployment後，informer 建立的 Service也被刪除"
 
-#sa=`for f in ./manifest/*; do cat ${f} | yq '(.|select(.kind == "ServiceAccount")).metadata.name' ; done`
-#deployment=`for f in ./manifest/*; do cat ${f} | yq "(.|select(.spec.template.spec.serviceAccountName == \"${sa}\")).metadata.name" ; done`
 
 deployment=nginx-deployment
 kubectl delete deployments.apps ${deployment} >/dev/null  2>&1
 
 LABEL="ntcu-k8s=hw3"
 
-svc_num=`kubectl get svc   -l ${LABEL}  -o yaml | yq '.items | length'`
 
+for i in {1..20}; do
+  sleep 1
+  svc_num=`kubectl get svc   -l ${LABEL}  -o yaml | yq '.items | length'`
+  if [[ "$svc_num" -eq 0 ]]; then
+      break
+  fi
 
-if [[ "$svc_num" -ne 0 ]]; then
-    echo "informer 刪除建立的svc, 應為0,  $svc_num 不正確"
-    exit 1
-fi
+  if [[ "$i" -eq 20 ]]; then
+      echo "timeout: informer 刪除建立的svc, 應為0,  $svc_num 不正確"
+      exit 1
+  fi
 
-#deployment_num=`kubectl get deployment -l ${LABEL}  -o yaml | yq '.items | length'`
-#if [[ "$deployment_num" -ne 0 ]]; then
-#    echo "client建立的deployment 數量 $deployment_num 不正確"
-#    exit 1
-#fi
+done
 
 echo "........ PASS"
