64ec67ec26677d7bd30e7cd8721b57bed97827da
XIEYIZHEN <43402142+XIEYIZHEN@users.noreply.github.com>
2022-06-18 18:37:57 +0800

    Update main.go

--- cmd/informer/main.go
+++ cmd/informer/main.go
@@ -5,12 +5,8 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"path"
 	"syscall"
 
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 )
 
@@ -21,36 +17,10 @@ var (
 func main() {
 	outsideCluster := flag.Bool("outside-cluster", false, "set to true when run out of cluster. (default: false)")
 	flag.Parse()
-	var clientset *kubernetes.Clientset
-	if *outsideCluster {
-		// creates the out-cluster config
-		home, err := os.UserHomeDir()
-		if err != nil {
-			panic(err)
-		}
-		config, err := clientcmd.BuildConfigFromFlags("", path.Join(home, ".kube/config"))
-		if err != nil {
-			panic(err.Error())
-		}
-		// creates the clientset
-		clientset, err = kubernetes.NewForConfig(config)
-		if err != nil {
-			panic(err.Error())
-		}
-	} else {
-		// creates the in-cluster config
-		config, err := rest.InClusterConfig()
-		if err != nil {
-			panic(err.Error())
-		}
-		// creates the clientset
-		clientset, err = kubernetes.NewForConfig(config)
-		if err != nil {
-			panic(err.Error())
-		}
-	}
 
-	controller := NewDeploymentController(clientset)
+	clientset := GetClientSet(*outsideCluster)
+
+	controller := DeploymentController(clientset)
 	stop := make(chan struct{})
 	defer close(stop)
 	err := controller.Run(stop)
@@ -63,4 +33,3 @@ func main() {
 	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 	<-stopChan
 }
-
