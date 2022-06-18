da7ad6b248a5feb59a342c843630d182e2ce873b
YANGshujun1110 <43403122+YANGshujun1110@users.noreply.github.com>
2022-06-18 23:00:08 +0800

    Update main.go

--- cmd/informer/main.go
+++ cmd/informer/main.go
@@ -1,4 +1,3 @@
-
 package main
 import (
     "context"
@@ -69,6 +68,20 @@ func main() {
 }
 func int32Ptr(i int32) *int32 { return &i }
 
+func GetService(client kubernetes.Interface, namespace, name string) (*corev1.Service, error){
+    cm, err := client.
+            CoreV1().
+            Services(namespace).
+            Get(
+                  context.Background(),
+                  name,
+                  metav1.GetOptions{},
+    if err != nil {
+        return nil, err
+    }
+    return cm, nil
+}
+
 func deleteService(client kubernetes.Interface, namespace, name string) error {
     err := client.
         CoreV1().
@@ -82,38 +95,17 @@ func deleteService(client kubernetes.Interface, namespace, name string) error {
 }
 var portnum int32 = 80
 func createService(client kubernetes.Interface, namespace, name string) (*corev1.Service, error) {
-    cm := &corev1.Service{
-        ObjectMeta: metav1.ObjectMeta{
-            Name: "cm-service",
-            Labels: map[string]string{
-                "ntcu-k8s": "hw3",
-            },
-        },
-        Spec: corev1.ServiceSpec{
-            Selector: map[string]string{
-                "ntcu-k8s": "hw3",
-            },
-            Type: corev1.ServiceTypeNodePort,
-            Ports: []corev1.ServicePort{
-                {
-                    Name:       "http",
-                    Port:       80,
-                    TargetPort: intstr.IntOrString{IntVal: portnum},
-                    NodePort:   30100,
-                    Protocol:   corev1.ProtocolTCP,
-                },
-            },
-        },
-    }
+    cm := &corev1.Service{Data: map[string]string{"ntcu-k8s": "hw3"}}
     cm.Namespace = namespace
     cm.Name = name
     cm, err := client.
         CoreV1().
-        Services(namespace).Create(
-        context.Background(),
-        cm,
-        metav1.CreateOptions{},
-    )
+        Services(namespace).
+        Create(
+               context.Background(),
+               cm,
+               metav1.CreateOptions{},
+        )
     if err != nil {
         return nil, err
     }
