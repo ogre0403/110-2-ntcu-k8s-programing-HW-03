e1b59635ff2ec8c6e4d357bcf5eb39a1d12d7a58
YANGshujun1110 <43403122+YANGshujun1110@users.noreply.github.com>
2022-06-18 23:50:46 +0800

    Update main.go

--- cmd/informer/main.go
+++ cmd/informer/main.go
@@ -12,7 +12,7 @@ import (
     //appv1 "k8s.io/api/apps/v1"
     corev1 "k8s.io/api/core/v1"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    //"k8s.io/apimachinery/pkg/util/intstr"
+    "k8s.io/apimachinery/pkg/util/intstr"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
     "k8s.io/client-go/tools/clientcmd"
