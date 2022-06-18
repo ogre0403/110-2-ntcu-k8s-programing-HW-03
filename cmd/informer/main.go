f087d8834b92d10408b80cd6506b3523e56fc2bf
YANGshujun1110 <43403122+YANGshujun1110@users.noreply.github.com>
2022-06-18 13:57:36 +0800

    Update main.go

--- cmd/informer/main.go
+++ cmd/informer/main.go
@@ -23,33 +23,14 @@ var (
 func main() {
     outsideCluster := flag.Bool("outside-cluster", false, "set to true when run out of cluster. (default: false)")
     flag.Parse()
-    if *outsideCluster {
-        // creates the out-cluster config
-        home, err := os.UserHomeDir()
-        if err != nil {
-            panic(err)
-        }
-        config, err := clientcmd.BuildConfigFromFlags("", path.Join(home, ".kube/config"))
-        if err != nil {
-            panic(err.Error())
-        }
-        // creates the clientset
-        clientset, err = kubernetes.NewForConfig(config)
-        if err != nil {
-            panic(err.Error())
-        }
-    } else {
-        // creates the in-cluster config
-        config, err := rest.InClusterConfig()
-        if err != nil {
-            panic(err.Error())
-        }
-        // creates the clientset
-        clientset, err = kubernetes.NewForConfig(config)
-        if err != nil {
-            panic(err.Error())
-        }
-    }
+    clientset := util.GetClientSet(*outsideCluster)
+    controller := NewDeploymentController(clientset)
+    stop := make(chan struct{})
+	defer close(stop)
+	err := controller.Run(stop)
+	if err != nil {
+		klog.Fatal(err)
+	}
     
     fmt.Println("Waiting for Kill Signal...")
     var stopChan = make(chan os.Signal, 1)
