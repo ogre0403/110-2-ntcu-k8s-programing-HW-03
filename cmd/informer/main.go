ef2dc06cc2b9ad386df76db8288d0cdbd9efd638
YANGshujun1110 <43403122+YANGshujun1110@users.noreply.github.com>
2022-06-18 12:43:55 +0800

    Update main.go

--- cmd/informer/main.go
+++ cmd/informer/main.go
@@ -1,4 +1,3 @@
-
 package main
 import (
     "context"
@@ -19,11 +18,11 @@ import (
 )
 var (
     namespace = "default"
+    clientset *kubernetes.Clientset
 )
 func main() {
     outsideCluster := flag.Bool("outside-cluster", false, "set to true when run out of cluster. (default: false)")
     flag.Parse()
-    var clientset *kubernetes.Clientset
     if *outsideCluster {
         // creates the out-cluster config
         home, err := os.UserHomeDir()
