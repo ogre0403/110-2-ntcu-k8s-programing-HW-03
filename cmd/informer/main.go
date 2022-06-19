aabe18895e44349b04d8686b4553568fc741c101
YANGshujun1110 <43403122+YANGshujun1110@users.noreply.github.com>
2022-06-19 12:57:13 +0800

    Update main.go

--- cmd/informer/main.go
+++ cmd/informer/main.go
@@ -8,7 +8,7 @@ import (
     "path"
     "syscall"
     //"time"
-    //appv1 "k8s.io/api/apps/v1"
+    appv1 "k8s.io/api/apps/v1"
     corev1 "k8s.io/api/core/v1"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     "k8s.io/apimachinery/pkg/util/intstr"
